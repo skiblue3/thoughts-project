6cd25b4348140e81cb703a2f0deca44a9612f0b7
skiblue3 <myzticblue126@gmail.com>
2022-04-23 00:11:01 +0530

    added build file for frontend

---
+++ build/precache-manifest.1e7e6c5ffd67d36279c49a866ab8654e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f785ecc5a843c81e8bf6ec9a8ebefddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b423df12a1d0c0f659bb",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "c32686090d0d4ae96c46",
+    "url": "/static/js/2.f1b46d8f.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.f1b46d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b423df12a1d0c0f659bb",
+    "url": "/static/js/main.b37aaf26.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/thoughts.5c0c63fb.png"
+  }
+]);
--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use(express.static('build'))
+
 app.use('/posts', postRoutes);
 
 app.get('/', (req, res) => {
